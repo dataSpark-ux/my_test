c2b3bc745425d8ef5927d1e0da8f35d4c94e27b0
837466309@qq.com <wangyi123>
2018-09-10 18:21:02 +0800

    添加aop处理日志以及redis配置

---
+++ src/main/java/com/wy/newblog/entity/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.wy.newblog.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.wy.newblog.common.utils.SnowFlakeUtil;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+/**
+ * @author wy
+ * @Date 2018/9/10 14:38
+ * @Description
+ * @Version 1.0
+ */
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value={"hibernateLazyInitializer","handler","fieldHandler"})
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @ApiModelProperty(value = "唯一标识")
+    private String id = String.valueOf(SnowFlakeUtil.getFlowIdInstance().nextId());
+
+    @ApiModelProperty(value = "创建者")
+    private String createBy;
+
+    @ApiModelProperty(value = "创建时间")
+    private Long createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Long updateTime;
+}
--- src/main/java/com/wy/newblog/entity/UserEntity.java
+++ src/main/java/com/wy/newblog/entity/UserEntity.java
@@ -1,148 +1,38 @@
 package com.wy.newblog.entity;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
+@Data
 @Entity
 @Table(name = "t_user")
-public class UserEntity {
-    @Id
-    @GeneratedValue
-    private long id;
+public class UserEntity extends BaseEntity {
 
     @Column(nullable = false)
     @NotNull(message = "用户名不能为空")
     @Size(max = 15, min = 8, message = "用户长度最少大于8且小于15")
+    @ApiModelProperty(value = "用户名称")
     private String username;
 
     @Column(nullable = false)
     @NotNull(message = "密码不能为空")
     @Size(max = 50, min = 8, message = "密码最少八位")
+    @ApiModelProperty(value = "密码")
     private String password;
-
+    @ApiModelProperty(value = "生日")
     private Date birthDate;
 
     @NotNull(message = "昵称不能为空")
+    @ApiModelProperty(value = "昵称")
     private String nickname;
-
+    @ApiModelProperty(value = "邮箱")
     private String email;
-
-    private String github;
-
-    private String qq;
-
-    private String wecaht;
-
+    @ApiModelProperty(value = "头像")
     private String avatar;
 
-    private Integer status;
 
-    private Long CreateTime;
-
-    private Long UpdateTime;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGithub() {
-        return github;
-    }
-
-    public void setGithub(String github) {
-        this.github = github;
-    }
-
-    public String getQq() {
-        return qq;
-    }
-
-    public void setQq(String qq) {
-        this.qq = qq;
-    }
-
-    public String getWecaht() {
-        return wecaht;
-    }
-
-    public void setWecaht(String wecaht) {
-        this.wecaht = wecaht;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Long getCreateTime() {
-        return CreateTime;
-    }
-
-    public void setCreateTime(Long createTime) {
-        CreateTime = createTime;
-    }
-
-    public Long getUpdateTime() {
-        return UpdateTime;
-    }
-
-    public void setUpdateTime(Long updateTime) {
-        UpdateTime = updateTime;
-    }
 }
